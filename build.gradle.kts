348c89a809e8a433d4b026494ec924937fc7ba09
sad25kag <sad25kag@gmail.com>
2026-07-02 21:11:40 +0700

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,12 @@ buildscript {
     val kotlinVersion = project
         .findProperty("kotlin.version")
         ?.toString()
-        ?: "2.3.0"
+        ?: "2.4.0"
 
     val androidGradlePluginVersion = project
         .findProperty("android.gradle.plugin.version")
         ?.toString()
-        ?: "9.1.0"
+        ?: "9.1.1"
 
     repositories {
         google()
