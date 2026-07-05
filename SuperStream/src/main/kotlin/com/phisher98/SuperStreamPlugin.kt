d9606eeb2bccceda2ddb12ba60dbb84ddd463204
sad25kag <sad25kag@gmail.com>
2026-07-05 18:20:26 +0700

    update

---
+++ SuperStream/src/main/kotlin/com/phisher98/SuperStreamPlugin.kt
@@ -0,0 +1,21 @@
+﻿package com.sad25kag
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.sad25kag.settings.SettingsFragment
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class SuperStreamPlugin: Plugin() {
+    override fun load(context: Context) {
+        val sharedPref = context.getSharedPreferences("SuperStream", Context.MODE_PRIVATE)
+        registerMainAPI(SuperStream(sharedPref))
+
+        openSettings = { ctx ->
+            val activity = ctx as AppCompatActivity
+            val frag = SettingsFragment(this, sharedPref)
+            frag.show(activity.supportFragmentManager, "Frag")
+        }
+    }
+}
