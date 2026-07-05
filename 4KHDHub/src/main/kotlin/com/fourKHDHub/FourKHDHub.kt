d253290b1fbab4fe20c7358230c8a65ea8e3dca7
sad25kag <sad25kag@gmail.com>
2026-07-06 02:23:00 +0700

    update

--- 4KHDHub/src/main/kotlin/com/fourKHDHub/FourKHDHub.kt
+++ 4KHDHub/src/main/kotlin/com/fourKHDHub/FourKHDHub.kt
@@ -5,6 +5,7 @@ import androidx.annotation.RequiresApi
 import com.lagradost.api.Log
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.Extensions.toSearchResult
 import com.lagradost.cloudstream3.Episode
 import com.lagradost.cloudstream3.LoadResponse.Companion.addSimklId
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
