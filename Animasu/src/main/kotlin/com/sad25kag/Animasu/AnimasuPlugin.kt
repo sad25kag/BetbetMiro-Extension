66769a583f9a3f6f881d8a4e25c9bef28bf2cee2
sad25kag <sad25kag@gmail.com>
2026-07-08 08:33:57 +0700

    Fix: commit sebelumnya salah kirim placeholder, bukan isi file asli

--- Animasu/src/main/kotlin/com/sad25kag/Animasu/AnimasuPlugin.kt
+++ Animasu/src/main/kotlin/com/sad25kag/Animasu/AnimasuPlugin.kt
@@ -1,1 +1,21 @@
-PLACEHOLDER
+package com.sad25kag.Animasu
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class AnimasuPlugin : Plugin() {
+
+    override fun load(context: Context) {
+
+        registerMainAPI(Animasu())
+
+        registerExtractorAPI(Archivd())
+        registerExtractorAPI(Newuservideo())
+        registerExtractorAPI(Vidhidepro())
+        registerExtractorAPI(Gdplayer())
+        registerExtractorAPI(BerkasDrive())
+        registerExtractorAPI(Mitedrive())
+    }
+}
