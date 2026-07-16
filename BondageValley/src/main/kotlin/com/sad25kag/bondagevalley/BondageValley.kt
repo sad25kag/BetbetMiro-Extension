1a7581788a937214da3d979a763556da106772d9
sad25kag <sad25kag@gmail.com>
2026-07-16 12:07:08 +0700

    refactor(BondageValley): remove models from main pages
    - Remove Models menu from mainPage
    - Keep video listing pages unchanged
    - Simplify available catalog options

--- BondageValley/src/main/kotlin/com/sad25kag/bondagevalley/BondageValley.kt
+++ BondageValley/src/main/kotlin/com/sad25kag/bondagevalley/BondageValley.kt
@@ -27,7 +27,6 @@ class BondageValley : MainAPI() {
         "$mainUrl" to "Semua Video",
         "$mainUrl/videos/latest" to "Video Terbaru",
         "$mainUrl/videos/top" to "Video Terbaik",
-        "$mainUrl/videos/model" to "Models"
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
